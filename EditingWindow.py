0fc68ad32eda1c7ba0a6327cb4d9149a1d155a27
James Robertson <metamolejr@gmail.com>
2021-12-05 20:20:30 +0000

    Commenting and minor adjustments

--- EditingWindow.py
+++ EditingWindow.py
@@ -6,11 +6,20 @@ import random
 
 class EditingPage(Tk):
     def __init__(self, *args, **kwargs):
+        """
+         - The innit function is used to define key values such as width and height of the window
+        - It also involves defining the connection and cursor, which allow the accessing and executing of cammands on the
+          database
+        - The other main function of the __init__ function is to define and place all the gui widget
+        - there is also an information message sent to the user, advising
+
+        """
         super().__init__(*args, **kwargs)
         self.geometry = "400x400"
         self.title("Db GUI")
         self.con = sqlite3.connect("CoffeeShop.db")
         self.cur = self.con.cursor()
+        # options contains the three options for the user
         self.options = ["Add", "Edit", "Delete"]
         self.drop_down_value = StringVar(self)
         self.drop_down_value.set("Edit")
@@ -42,12 +51,18 @@ class EditingPage(Tk):
         self.submit_button.place(x=100, y=250)
         self.instructions_button.place(x=170, y=250)
 
-        msgbx.showinfo("", """For ADD - Enter the username and password that you want to add, leave the customer ID blank as it will be generated for you - seperate multiple values with commas
+        # information for the user on how to use the program
+        msgbx.showinfo("", """For ADD - Enter the username and password that you want to add, leave the customer ID blank as it will be generated for you 
+                                      - seperate multiple values with commas
                               FOR EDIT - ENTER updated username and password and enter the Customer ID of the row you want to change
                               FOr Delete - enter the customer ID of the row to  be deleted""")
 
     # checks the mode that the user wants to use
     def check_mode(self):
+        """
+        - this function is run when the submit button is pressed
+        - it will run a function based on the option selected by the user
+        """
         mode = self.drop_down_value.get()
         if mode == "Edit":
             self.edit_data()
@@ -58,15 +73,26 @@ class EditingPage(Tk):
 
     # Adding Data
     def add_data(self):
+        """
+        - this function adds data to the table UserLogin
+        - it has a try, except clause to allow for multiple insertions at once separated by commas
+        - this allows an error from the split() function to be caught and for the single entry code to be applied
+        - each triplet of data is added iteratively to the database
+
+        """
         try:
             sql = f"INSERT INTO UserLogin(Username, Password, CustomerID) Values(?,?,?)"
+            # this pulls the data from the inputs and converts it to strings to allow for string manipulation
             data = (str(self.Username_entry.get()), str(self.Password_entry.get()), random.randint(1, 1000000000000))
+            # this creates lists of each username and password inputted by the user
             usernames = data[0].split(",")
             passwords = data[1].split(",")
+            # this loops through, inputting each row in the tables
             for i in range(len(usernames)):
                 data = (str(usernames[(i-1)]), str(passwords[i-1]), random.randint(1, 1000000000000))
                 self.cur.execute(sql, data)
         except Exception:
+            # this inputs a single row into the table
             sql = f"INSERT INTO UserLogin(Username, Password, CustomerID) Values(?,?,?)"
             data = (str(self.Username_entry.get()), str(self.Password_entry.get()), random.randint(1, 1000000000000))
             self.cur.execute(sql, data)
@@ -75,14 +101,24 @@ class EditingPage(Tk):
 
     # Deleting Data
     def delete_data(self):
+        """
+        - this function allows the deleting of rows of data
+        - it takes the customer ID (primary key) and deletes the corresponding rows
+        - it can handle both single and multiple row deletions using the try, execpt clause
+        """
         try:
+            # used for multiple deletions
             sql = f"DELETE FROM UserLogin WHERE CustomerID=?"
+            # the customer ID is retrieved from the input and is converted to a string
             data = (str(self.CustomerID_entry.get()))
+            # a list of customer IDs is created
             Customer_IDs = data.split(",")
+            # each customer ID is iterated over and the row is deleted
             for i in range(len(Customer_IDs)):
                 data = (str(Customer_IDs[(i-1)]),)
                 self.cur.execute(sql, data)
         except Exception:
+            # used for single deletioncs
             sql = f"DELETE FROM UserLogin WHERE CustomerID=?"
             data = (str(self.CustomerID_entry.get()),)
             self.cur.execute(sql, data)
@@ -91,16 +127,26 @@ class EditingPage(Tk):
 
     # Edit data
     def edit_data(self):
+        """
+        - this function allows for the editing of multiple rows
+        - you can change the username and password of a user by inputting the username, password and customer ID
+        - it can handle multiple or single updates
+        - this is enabled by the try and except clause
+        """
         try:
             sql = "UPDATE UserLogin SET Username=?, Password=? WHERE CustomerID=?"
-            data = (str(self.Username_entry.get()), str(self.Password_entry.get()), self.CustomerID_entry.get())
+            # this retrieves the data and converts each one to a string
+            data = (str(self.Username_entry.get()), str(self.Password_entry.get()), str(self.CustomerID_entry.get()))
+            # these create lists of the Username, Password and CustomerID
             usernames = data[0].split(",")
             passwords = data[1].split(",")
             Customer_IDs = data[2].split(",")
+            # each set of values is iterated over and the rows are each updated
             for i in range(len(usernames)):
                 data = (str(usernames[(i-1)]), str(passwords[i-1]), str(Customer_IDs[i-1]))
                 self.cur.execute(sql, data)
         except Exception:
+            # this handles the single row updates
             sql = "UPDATE UserLogin SET Username=?, Password=? WHERE CustomerID=?"
             data = (str(self.Username_entry.get()), str(self.Password_entry.get()), self.CustomerID_entry.get())
             self.cur.execute(sql, data)
@@ -108,6 +154,9 @@ class EditingPage(Tk):
         msgbx.showinfo("", "DATA SUCCESSFULLY UPDATED")
 
     def show_instructions(self):
+        """
+        - this function creates a messagebox to instruct the user on how to user the program
+        """
         msgbx.showinfo("", """For ADD - Enter the username and password that you want to add, leave the customer ID blank as it will be generated for you
                               FOR EDIT - ENTER updated username and password and enter the Customer ID of the row you want to change
                               FOr Delete - enter the customer ID of the row to  be deleted
--- LoginWindow.py
+++ LoginWindow.py
@@ -1,25 +1,31 @@
 import random
 from tkinter import *
 from tkinter import messagebox as msgbx
-from MainWindow import EditingPage
+from EditingWindow import EditingPage
 import sqlite3
 
 
 class LoginWindow(Tk):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.geometry = "400x400"
-        self.title("Db GUI")
-        self.con = sqlite3.connect("CoffeeShop.db") # connection to the database
-        self.cur = self.con.cursor()
+        """
+        - The innit function is used to define key values such as width and height of the window
+        - It also involves defining the connection and cursor, which allow the accessing and executing of cammands on the
+          database
+        - The other main function of the __init__ function is to define and place all the gui widget
+        """
+        super().__init__(*args, **kwargs)  # this initialises the inherited gui class
+        self.geometry = "400x400"  # this defines the WidthxHeight
+        self.title("Db GUI")  # the title of the gui
+        self.con = sqlite3.connect("CoffeeShop.db")  # connection to the database
+        self.cur = self.con.cursor()  # the object used to execute sql commands
 
         # the base frame of the first window
 
-        self.frame = Frame(self, width=400, height=400)
-        self.frame.pack()
+        self.frame = Frame(self, width=400, height=400)  # defining the frame
+        self.frame.pack()  # placing the frame
 
-        # widgets for the gui
+        # Defining each widget
 
         self.Title_label = Label(self.frame, text="Welcome To The Coffee Shop Db")
         self.username_label = Label(self.frame, text="username:")
@@ -40,9 +46,18 @@ class LoginWindow(Tk):
         self.login_button.place(x=300, y=300)
 
     def sign_up(self):
+        """
+        - this functions main goal is to handle the signing up of users
+        - it is the function called by pressing the sign_up_button
+        - First, it checks if either the username entry or the password entry are empty
+          if so, it will cause a message box to pop up displaying an error
+        - then the username and password are inserted into the database
+        """
         if self.username_entry.get() == "" or self.password_entry.get() == "":
+            # error message being shown
             msgbx.showerror("", "The Username or Password is blank - please retry")
         else:
+            # sql command being executed
             self.cur.execute(
                 f"INSERT INTO UserLogin(Username, Password, CustomerID) VALUES (?,?,?)",
                 (self.username_entry.get(), self.password_entry.get(), random.randint(1, 1000000)))
@@ -50,13 +65,24 @@ class LoginWindow(Tk):
             msgbx.showinfo("", "You Are Signed Up")
 
     def sign_in(self):
+        """
+        - this function handles the signing in of a user
+        - it does an initial check of both inputs to check if they are not empty
+        - then, all the entries in the UserLogin table are retrieved
+        - they are iteratively compared to the Username and Password
+        - if there is a match, the user passes authentication and a new window is created
+        -  if there is no match, a message is sent to the user, saying that they have been denied
+        """
         if self.username_entry.get() == "" or self.password_entry.get() == "":
+            # error message being displayed
             msgbx.showerror("", "The Username or Password is blank - please retry")
         else:
             self.checkInDb(username=self.username_entry.get(), password=self.password_entry.get())
 
     def checkInDb(self, username, password):
+        # selecting all rows in user login and looping through them
         for row in self.cur.execute("SELECT * FROM UserLogin"):
+            # indexing the list returned from the the line above to access the username and password
             if row[1] == username and row[2] == password:
                 msgbx.showinfo("", "SIGN IN SUCCESSFUL")
                 self.con.close()
